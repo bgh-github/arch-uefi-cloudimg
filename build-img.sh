0dbc3d1f4566fff99cce049b6550c51024ffe4e1
bgh <git@bgh.io>
2023-08-05 15:30:00 +1000

    Implement UKI booting. ESP mounted to /efi

--- build-img.sh
+++ build-img.sh
@@ -13,30 +13,28 @@ sudo mkfs.fat -F 32 "${loop_dev}p1"
 sudo mkfs.ext4 "${loop_dev}p2"
 sudo partprobe "${loop_dev}"
 
-sudo mount --mkdir "${loop_dev}p1" "${output_dir}/mount/boot"
+sudo mount --mkdir -o umask=077 "${loop_dev}p1" "${output_dir}/mount/efi"
 sudo mount --mkdir "${loop_dev}p2" "${output_dir}/mount/img"
-sudo mount --mkdir --bind "${output_dir}/mount/boot" "${output_dir}/mount/img/boot"
-sudo pacstrap "${output_dir}/mount/img" base linux zram-generator openssh cloud-init cloud-guest-utils gptfdisk
+sudo pacstrap "${output_dir}/mount/img" base
+sudo arch-chroot "${output_dir}/mount/img" mkdir /efi
+sudo mount --bind "${output_dir}/mount/efi" "${output_dir}/mount/img/efi"
+sudo pacstrap "${output_dir}/mount/img" linux zram-generator openssh cloud-init cloud-guest-utils gptfdisk
 
-sudo arch-chroot "${output_dir}/mount/img" /bin/bash << EOM
+sudo arch-chroot "${output_dir}/mount/img" /bin/bash << EOF
+rm /boot/initramfs-linux*.img
 echo '[zram0]' > /etc/systemd/zram-generator.conf
 ln --symbolic --force /usr/share/zoneinfo/UTC /etc/localtime
 systemctl enable systemd-networkd.service systemd-resolved.service cloud-init.service cloud-final.service
-sed --in-place --expression='s|\(^\MODULES=(\)\(.*\))$|\1\2 virtio-pci virtio-scsi)|' --expression='s|(\s|(|' /etc/mkinitcpio.conf
-sed --in-place --expression='s|\(^HOOKS=(base\)|\1 systemd|' /etc/mkinitcpio.conf
-mkinitcpio --allpresets
+sed --in-place --expression='s|\(^\MODULES=(\)\(.*\))$|\1\2 virtio-pci virtio-scsi)|' --expression='s|(\s|(|' --expression='s|\(^HOOKS=(base\)|\1 systemd|' /etc/mkinitcpio.conf
+sed --in-place --expression='s|\(default_image=\)|#\1|' --expression='s|#\(default_uki=\)|\1|' --expression='s|\(fallback_image=\)|#\1|' --expression='s|#\(fallback_uki=\)|\1|' --expression='s|#\(default_options=\)|\1|' /etc/mkinitcpio.d/linux.preset
 bootctl install
-cat > "/boot/loader/entries/$(grep --perl-regexp --only-matching '^ID=\K.*' /etc/os-release).conf" << EOF
-title $(grep '^PRETTY_NAME=' /etc/os-release | sed --expression='s|^PRETTY_NAME="\(.*\)"$|\1|')
-linux /vmlinuz-linux
-initrd /initramfs-linux.img
-EOF
+mkinitcpio --allpresets
 sed --in-place --expression='s|.*|uninitialized|' /etc/machine-id
 echo "BUILD_ID=${output_file}-$(date --utc --iso-8601=minutes)" >> /etc/os-release
-EOM
+EOF
 
-sudo umount "${output_dir}/mount/img/boot" && sudo rmdir "${output_dir}/mount/img/boot"
-sudo umount "${output_dir}/mount/boot" && sudo rmdir "${output_dir}/mount/boot"
+sudo umount "${output_dir}/mount/img/efi" && sudo rmdir "${output_dir}/mount/img/efi"
+sudo umount "${output_dir}/mount/efi" && sudo rmdir "${output_dir}/mount/efi"
 sudo umount "${output_dir}/mount/img" && sudo rmdir "${output_dir}/mount/img" && sudo rmdir "${output_dir}/mount"
 
 qemu-img convert -c -O qcow2 "${output_path}" "${output_path}.qcow2"
