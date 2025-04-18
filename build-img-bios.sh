3f5baa679a3f3c400279fee68067e2ea4d511e7a
bgh <git@bgh.io>
2025-04-18 22:44:59 +1000

    Better align BIOS variant with main build

--- build-img-bios.sh
+++ build-img-bios.sh
@@ -5,20 +5,21 @@ output_dir=$(dirname "${output_path}")
 output_file=$(basename "${output_path}")
 
 truncate --size=2G "${output_path}"
-sgdisk --align-end --clear --new 0:0:+1M --typecode=0:ef02 --new 0:0:0 --typecode=0:8304 "${output_path}"
+echo -e 'label: gpt\n size=1MiB, type=21686148-6449-6e6f-744e-656564454649\n type=4f68bce3-e8cd-4db1-96e7-fbcaf984b709, attrs=59' | sfdisk "${output_path}"
 
 loop_dev=/dev/loop123
 sudo losetup --partscan "${loop_dev}" "${output_path}"
 sudo mkfs.ext4 "${loop_dev}p2"
 
 sudo mount --mkdir "${loop_dev}p2" "${output_dir}/mount/img"
-sudo pacstrap "${output_dir}/mount/img" base linux zram-generator openssh cloud-init cloud-guest-utils grub
+sudo pacstrap "${output_dir}/mount/img" base linux zram-generator openssh cloud-init grub
 
 sudo arch-chroot "${output_dir}/mount/img" /bin/bash << EOF
 echo '[zram0]' > /etc/systemd/zram-generator.conf
+mkdir /etc/repart.d && echo -e '[Partition]\nType=root' > /etc/repart.d/grow-root.conf
 systemd-firstboot --timezone=UTC
-systemctl enable systemd-networkd.service systemd-resolved.service cloud-init.service cloud-final.service
-sed --in-place --expression='s|\(^\MODULES=(\)\(.*\))$|\1\2 virtio_pci sr_mod)|' --expression='s|(\s|(|' --expression='s|\(^HOOKS=(base\)|\1 systemd|' /etc/mkinitcpio.conf
+systemctl enable systemd-networkd.service systemd-resolved.service cloud-init-main.service cloud-final.service
+sed --in-place --expression='s|\(^\MODULES=\).*|\1(virtio_pci sr_mod)|' --expression='s|\(^HOOKS=(base\)|\1 systemd|' --expression='/^HOOKS=/s| udev||; /^HOOKS=/s| keymap||; /^HOOKS=/s| consolefont||' /etc/mkinitcpio.conf
 mkinitcpio --allpresets
 grub-install --target=i386-pc "${loop_dev}"
 grub-mkconfig --output=/boot/grub/grub.cfg
